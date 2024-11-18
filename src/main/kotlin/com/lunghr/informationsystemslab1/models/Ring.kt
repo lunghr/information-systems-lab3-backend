bbc742de393c956850478198cdb236677aa73d43
lunghr <myilanka8@gmail.com>
2024-11-18 18:13:58 +0300

    fix: linter

--- src/main/kotlin/com/lunghr/informationsystemslab1/models/Ring.kt
+++ src/main/kotlin/com/lunghr/informationsystemslab1/models/Ring.kt
@@ -1,8 +1,11 @@
-@file:Suppress("ktlint:standard:no-wildcard-imports")
 
 package com.lunghr.informationsystemslab1.models
 
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "rings")
