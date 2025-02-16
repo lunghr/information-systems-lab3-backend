944d105a09d1c0e441104b7a823612ef5bad5954
lunghr <myilanka8@gmail.com>
2025-02-16 05:20:29 +0300

    feat(file imports): implement file parsing & separation for workers logic

--- src/main/kotlin/com/lunghr/informationsystemslab1/import/service/FileService.kt
+++ src/main/kotlin/com/lunghr/informationsystemslab1/import/service/FileService.kt
@@ -1,22 +1,14 @@
 package com.lunghr.informationsystemslab1.import.service
 
-import com.lunghr.informationsystemslab1.dto.BookCreatureDto
-import com.lunghr.informationsystemslab1.dto.CoordinatesDto
-import com.lunghr.informationsystemslab1.dto.MagicCityDto
-import com.lunghr.informationsystemslab1.dto.RingDto
-import com.lunghr.informationsystemslab1.import.exceptions.InvalidFileDataException
 import com.lunghr.informationsystemslab1.service.BookCreatureService
 import com.lunghr.informationsystemslab1.service.MagicCityService
 import com.lunghr.informationsystemslab1.service.RingService
-import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.xssf.usermodel.XSSFSheet
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
+import java.util.concurrent.ForkJoinPool
 
 @Service
 @Transactional(rollbackFor = [Exception::class])
@@ -24,126 +16,74 @@ class FileService(
     private val magicCityService: MagicCityService,
     private val ringService: RingService,
     private val bookCreatureService: BookCreatureService,
-//    private val taskExecutor: ThreadPoolTaskExecutor
+    private val forkJoinPool: ForkJoinPool
 ) {
     @Transactional(rollbackFor = [Exception::class])
     fun importObjectsFromFiles(files: List<MultipartFile>, token: String) {
-        val executor = Executors.newFixedThreadPool(4)
         val futures = files.map { file ->
-            executor.submit(Callable { processFileWithTimeout(file, token) })
-        }
-
-        try {
-            futures.forEach { it.get() }
-        } catch (e: Exception) {
-            futures.forEach { it.cancel(true) }
-            throw e
-        } finally {
-            executor.shutdown()
-        }
-    }
-
-    private fun processFileWithTimeout(file: MultipartFile, token: String) {
-        val executor = Executors.newCachedThreadPool()
-        val future = executor.submit(Callable { processFile(file, token) })
-
-        try {
-            future.get(30, TimeUnit.SECONDS)
-        } catch (e: TimeoutException) {
-            future.cancel(true)
-            println("Файл обрабатывается слишком долго, увеличиваем число потоков")
-            processFileInParallel(file, token)
-        } finally {
-            executor.shutdown()
-        }
-    }
-
-    private fun processFile(file: MultipartFile, token: String) {
-        require(file.contentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") { "Invalid file type" }
-
-        file.inputStream.use { inputStream ->
-            val sheet = XSSFWorkbook(inputStream).getSheetAt(0)
-
-            val (rings, magicCities, bookCreatures) = sheet.groupBy { it.getCell(0).stringCellValue.trim() }
-                .let {
-                    Triple(it["Ring"].orEmpty(), it["MagicCity"].orEmpty(), it["BookCreature"].orEmpty())
-                }
-
-            rings.forEach { ringService.createRing(parseRingDto(it), token) }
-            magicCities.forEach { magicCityService.createMagicCity(parseMagicCityDto(it), token) }
-            bookCreatures.forEach { bookCreatureService.createBookCreature(parseBookCreatureDto(it), token) }
-        }
-    }
-
-    private fun processFileInParallel(file: MultipartFile, token: String) {
-        val executor = Executors.newFixedThreadPool(2) // Два потока на задачу
-        val future1 = executor.submit(Callable { processHalfFile(file, token, true) })
-        val future2 = executor.submit(Callable { processHalfFile(file, token, false) })
-
-        try {
-            future1.get()
-            future2.get()
-        } finally {
-            executor.shutdown()
-        }
-    }
-
-    private fun processHalfFile(file: MultipartFile, token: String, firstHalf: Boolean) {
-        file.inputStream.use { inputStream ->
-            val sheet = XSSFWorkbook(inputStream).getSheetAt(0)
-            val rows = sheet.iterator().asSequence().toList()
-            val half = rows.size / 2
-
-            val targetRows = if (firstHalf) rows.take(half) else rows.drop(half)
-
-            targetRows.forEach {
-                when (it.getCell(0).stringCellValue.trim()) {
-                    "Ring" -> ringService.createRing(parseRingDto(it), token)
-                    "MagicCity" -> magicCityService.createMagicCity(parseMagicCityDto(it), token)
-                    "BookCreature" -> bookCreatureService.createBookCreature(parseBookCreatureDto(it), token)
-                }
+            forkJoinPool.submit {
+                prepareFile(file)
             }
         }
+        futures.forEach { it.get() }
     }
 
-    private fun parseRingDto(row: Row): RingDto {
-        return runCatching {
-            RingDto(
-                name = row.getCell(1).stringCellValue,
-                weight = row.getCell(2).numericCellValue.toInt()
-            )
-        }.getOrElse { throw InvalidFileDataException("Invalid data in row ${row.rowNum}") }
+    @Transactional(rollbackFor = [Exception::class])
+    fun prepareFile(file: MultipartFile) {
+        println(file.originalFilename)
+        file.inputStream.use { inputStream ->
+            val workbook = XSSFWorkbook(inputStream)
+            val sheet = workbook.getSheetAt(0)
+            val rowCount = sheet.physicalNumberOfRows
+            if (rowCount == 0) {
+                return
+            }
+            val header = sheet.getRow(0)
+            val headerMap = mutableMapOf<String, Int>()
+            for (i in 0 until header.physicalNumberOfCells) {
+                headerMap[header.getCell(i).stringCellValue] = i
+            }
+            val startIndex = 1;
+            val endIndex = rowCount
+
+            println(headerMap)
+            println(startIndex)
+            println(endIndex)
+
+            processSheet(sheet, headerMap, startIndex, endIndex, 5)
+        }
     }
 
-    private fun parseMagicCityDto(row: Row): MagicCityDto {
-        return runCatching {
-            MagicCityDto(
-                name = row.getCell(1).stringCellValue,
-                area = row.getCell(2).numericCellValue,
-                population = row.getCell(3).numericCellValue.toInt(),
-                establishedData = java.time.LocalDateTime.now(),
-//                establishedData = row.getCell(4).dateCellValue.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
-                governor = row.getCell(5).stringCellValue,
-                capital = row.getCell(6).booleanCellValue,
-                populationDensity = row.getCell(7).numericCellValue
-            )
-        }.getOrElse { throw InvalidFileDataException("Invalid data in row ${row.rowNum}") }
-    }
+    @Transactional(rollbackFor = [Exception::class])
+    fun processSheet(sheet: XSSFSheet, headerMap: Map<String, Int>, startIndex: Int, endIndex: Int, depth: Int) {
+        val spawnTimestamp = System.currentTimeMillis()
+        val timeout = 3000
+        val forkJoinThreshold = 32
 
-    private fun parseBookCreatureDto(row: Row): BookCreatureDto {
-        return runCatching {
-            BookCreatureDto(
-                name = row.getCell(1).stringCellValue,
-                coordinates = CoordinatesDto(
-                    x = row.getCell(2).numericCellValue.toInt(),
-                    y = row.getCell(3).numericCellValue
-                ),
-                age = row.getCell(4).numericCellValue.toInt(),
-                creatureType = row.getCell(5).stringCellValue,
-                ringId = row.getCell(6).numericCellValue.toLong(),
-                creatureLocationId = row.getCell(7).numericCellValue.toLong(),
-                attackLevel = row.getCell(8).numericCellValue.toFloat()
-            )
-        }.getOrElse { throw InvalidFileDataException("Invalid data in row ${row.rowNum}") }
+        for (i in startIndex until endIndex) {
+            if (System.currentTimeMillis() - spawnTimestamp > timeout
+                && endIndex - i > forkJoinThreshold
+                && depth > 0
+            ) {
+                val half = (i + endIndex) / 2
+
+                println()
+                println()
+                val firstWorker = forkJoinPool.submit {
+                    processSheet(sheet, headerMap, i, half, depth - 1)
+                }
+                val secondWorker = forkJoinPool.submit {
+                    processSheet(sheet, headerMap, half, endIndex, depth - 1)
+                }
+
+                firstWorker.join()
+                secondWorker.join()
+
+                return
+            }
+
+            Thread.sleep(100)
+        }
+
     }
 }
